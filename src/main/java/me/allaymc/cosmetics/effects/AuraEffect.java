a289b1b384145d9f7cf1672b4d79ec5b48fb7313
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-04-26 00:17:22 +0530

    Update AuraEffect.java

--- src/main/java/me/allaymc/cosmetics/effects/AuraEffect.java
+++ src/main/java/me/allaymc/cosmetics/effects/AuraEffect.java
@@ -1,39 +1,14 @@
 package me.allaymc.cosmetics.effects;
 
-import org.bukkit.Particle;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
-public class AuraEffect implements CosmeticEffect {
-
-    private BukkitRunnable task;
-
-    @Override
-    public String getId() {
-        return "aura";
-    }
+public class AuraEffect extends CosmeticEffect {
 
     @Override
     public void enable(Player player) {
-        task = new BukkitRunnable() {
-            @Override
-            public void run() {
-                player.getWorld().spawnParticle(
-                        Particle.PORTAL,
-                        player.getLocation(),
-                        20,
-                        0.5, 1, 0.5
-                );
-            }
-        };
-        task.runTaskTimerAsynchronously(
-                player.getServer().getPluginManager().getPlugin("AllayCosmetics"),
-                0L, 10L
-        );
     }
 
     @Override
     public void disable(Player player) {
-        if (task != null) task.cancel();
     }
 }
