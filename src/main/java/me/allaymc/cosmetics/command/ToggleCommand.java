29335dbd6b3190c6a2d7bc3aba02cd6b33ae33d7
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-04-25 23:58:37 +0530

    Update ToggleCommand.java

--- src/main/java/me/allaymc/cosmetics/command/ToggleCommand.java
+++ src/main/java/me/allaymc/cosmetics/command/ToggleCommand.java
@@ -1,7 +1,9 @@
 package me.allaymc.cosmetics.command;
 
 import me.allaymc.cosmetics.manager.CosmeticManager;
-import org.bukkit.command.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class ToggleCommand implements CommandExecutor {
@@ -13,18 +15,16 @@ public class ToggleCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if (!(sender instanceof Player player)) return true;
 
-        if (args.length == 0) return false;
-
-        if (args[0].equalsIgnoreCase("on")) {
-            manager.enable(player);
-            player.sendMessage("§aCosmetics enabled.");
-        } else if (args[0].equalsIgnoreCase("off")) {
+        if (manager.isEnabled(player)) {
             manager.disable(player);
-            player.sendMessage("§cCosmetics disabled.");
+            player.sendMessage("§cCosmetics disabled");
+        } else {
+            manager.enable(player);
+            player.sendMessage("§aCosmetics enabled");
         }
 
         return true;
