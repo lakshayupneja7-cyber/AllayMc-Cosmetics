6787182dc729da548b2f0686cc82ae5c35496eb7
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-04-26 00:16:30 +0530

    Update AllayPetEffect.java

--- src/main/java/me/allaymc/cosmetics/effects/AllayPetEffect.java
+++ src/main/java/me/allaymc/cosmetics/effects/AllayPetEffect.java
@@ -1,33 +1,16 @@
 package me.allaymc.cosmetics.effects;
 
-import org.bukkit.entity.Allay;
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class AllayPetEffect implements CosmeticEffect {
-
-    private final Map<Player, Allay> pets = new HashMap<>();
+public class AllayPetEffect extends CosmeticEffect {
 
     @Override
-    public void start(Player p) {
-        Allay a = p.getWorld().spawn(p.getLocation(), Allay.class);
-        a.setAI(false);
-        a.setInvulnerable(true);
-        pets.put(p, a);
+    public void enable(Player player) {
+        // effect logic
     }
 
     @Override
-    public void stop(Player p) {
-        if (pets.containsKey(p)) {
-            pets.get(p).remove();
-            pets.remove(p);
-        }
-    }
-
-    @Override
-    public String getName() {
-        return "allay_pet";
+    public void disable(Player player) {
+        // cleanup logic
     }
 }
