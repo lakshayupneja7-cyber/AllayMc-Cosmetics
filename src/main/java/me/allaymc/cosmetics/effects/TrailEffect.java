4d7320583073d8040c8c4a0366441e0da587a829
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-04-26 00:17:08 +0530

    Update TrailEffect.java

--- src/main/java/me/allaymc/cosmetics/effects/TrailEffect.java
+++ src/main/java/me/allaymc/cosmetics/effects/TrailEffect.java
@@ -1,53 +1,14 @@
 package me.allaymc.cosmetics.effects;
 
-import org.bukkit.Particle;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public class TrailEffect implements CosmeticEffect {
-
-    private BukkitRunnable task;
+public class TrailEffect extends CosmeticEffect {
 
     @Override
-    public String getName() {
-        return "trail";
+    public void enable(Player player) {
     }
 
     @Override
-    public void start(Player player) {
-
-        stop(player);
-
-        JavaPlugin plugin = JavaPlugin.getProvidingPlugin(getClass());
-
-        task = new BukkitRunnable() {
-            @Override
-            public void run() {
-
-                if (!player.isOnline()) {
-                    cancel();
-                    return;
-                }
-
-                player.getWorld().spawnParticle(
-                        Particle.END_ROD,   // ✅ FIXED (replaces SPELL)
-                        player.getLocation().add(0, 0.1, 0),
-                        3,
-                        0.2, 0.2, 0.2,
-                        0.01
-                );
-            }
-        };
-
-        task.runTaskTimer(plugin, 0L, 5L);
-    }
-
-    @Override
-    public void stop(Player player) {
-        if (task != null) {
-            task.cancel();
-            task = null;
-        }
+    public void disable(Player player) {
     }
 }
