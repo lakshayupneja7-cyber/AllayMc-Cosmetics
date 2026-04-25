bb932c885fb8d180e79f155c15341310713c328f
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-04-26 00:17:44 +0530

    Update AllayCosmetics.java

--- src/main/java/me/allaymc/cosmetics/AllayCosmetics.java
+++ src/main/java/me/allaymc/cosmetics/AllayCosmetics.java
@@ -1,35 +1,19 @@
 package me.allaymc.cosmetics;
 
-import me.allaymc.cosmetics.command.CosmeticCommand;
-import me.allaymc.cosmetics.listener.JoinListener;
 import me.allaymc.cosmetics.manager.CosmeticManager;
 import me.allaymc.cosmetics.manager.RankManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AllayCosmetics extends JavaPlugin {
 
-    private static AllayCosmetics instance;
-
     private CosmeticManager cosmeticManager;
-    private RankManager rankManager;
 
     @Override
     public void onEnable() {
-        instance = this;
-
-        saveDefaultConfig();
-
-        rankManager = new RankManager(this);
-        cosmeticManager = new CosmeticManager(this, rankManager);
-
-        getServer().getPluginManager().registerEvents(new JoinListener(cosmeticManager), this);
-
-        getCommand("cosmetic").setExecutor(new CosmeticCommand(cosmeticManager));
-
-        getLogger().info("AllayCosmetics v3 enabled");
+        cosmeticManager = new CosmeticManager(this, new RankManager());
     }
 
-    public static AllayCosmetics getInstance() {
-        return instance;
+    public CosmeticManager getCosmeticManager() {
+        return cosmeticManager;
     }
 }
